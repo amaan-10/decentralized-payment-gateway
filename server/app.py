523d54f62f589e1453a83cc7a653e638aadc315d
Amaan <amaanshaikh.gg@gmail.com>
2025-05-15 20:04:20 +0530

    update

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 #app.py
 from flask import Flask
+from flask_cors import CORS
 from routes.auth_routes import auth_bp
 from routes.blockchain_routes import blockchain_bp
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000", "https://depayment.vercel.app"])
 
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
 app.register_blueprint(blockchain_bp, url_prefix="/api/blockchain")
