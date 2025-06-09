aeeffbf8819bac38ccb37ced8dd43a04cf066a29
Amaan <amaanshaikh.gg@gmail.com>
2025-06-09 22:54:56 +0530

    add: account api

--- server/app.py
+++ server/app.py
@@ -3,12 +3,14 @@ from flask import Flask
 from flask_cors import CORS
 from routes.auth_routes import auth_bp
 from routes.blockchain_routes import blockchain_bp
+from routes.accounts_routes import accounts_bp
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000", "https://depayment.vercel.app"])
 
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
 app.register_blueprint(blockchain_bp, url_prefix="/api/blockchain")
+app.register_blueprint(accounts_bp, url_prefix="/api/accounts")
 
 if __name__ == "__main__":
     app.run(debug=True)
