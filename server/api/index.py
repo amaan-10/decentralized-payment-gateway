fb09e1d2146350da5080e184b1395355efe25540
Amaan <amaanshaikh.gg@gmail.com>
2025-06-11 13:35:16 +0530

    update: files

--- server/api/index.py
+++ server/api/index.py
@@ -1,11 +1,12 @@
-#app.py
 from flask import Flask
 from flask_cors import CORS
+
 from routes.auth_routes import auth_bp
 from routes.blockchain_routes import blockchain_bp
 from routes.accounts_routes import accounts_bp
 
 app = Flask(__name__)
+
 CORS(app, 
      origins=["http://localhost:3000", "https://depayment.vercel.app"],
      supports_credentials=True,
@@ -16,5 +17,5 @@ app.register_blueprint(auth_bp, url_prefix="/api/auth")
 app.register_blueprint(blockchain_bp, url_prefix="/api/blockchain")
 app.register_blueprint(accounts_bp, url_prefix="/api/accounts")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def handler(environ, start_response):
+    return app(environ, start_response)
---
+++ server/run.py
@@ -0,0 +1,5 @@
+# run.py
+from api.index import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
