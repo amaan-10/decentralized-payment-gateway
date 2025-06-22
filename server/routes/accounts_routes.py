0e242c0e715448278114f8e1855ece8544b98e51
Amaan <amaanshaikh.gg@gmail.com>
2025-06-22 12:59:42 +0530

    update: spendless api

--- server/routes/accounts_routes.py
+++ server/routes/accounts_routes.py
@@ -4,6 +4,7 @@ from models.wallet import Wallet
 from models.transaction import Transaction
 from flask_cors import cross_origin
 from utils.jwt_utils import decode_token
+from db import wallets_collection
 
 accounts_bp = Blueprint('accounts', __name__)
 
@@ -97,4 +98,76 @@ def get_transactions():
         return jsonify({
             "success": False,
             "error": "No transactions found for this account"
+        }), 404
+    
+@accounts_bp.route("/connect/spendless", methods=["POST", "OPTIONS"])
+@cross_origin(
+    origins=["http://localhost:3000", "https://depayment.vercel.app"],
+    supports_credentials=True
+)
+def connect_spendless():
+    data = request.get_json()
+    token = request.headers.get("Authorization").split(" ")[1]
+    decoded = decode_token(token)
+
+    account_number = decoded["account_number"]
+    total_earned = data.get("totalEarned")
+    current_points = data.get("currentPoints")
+    link_spendless = data.get("linkSpendLess")
+
+
+    if not account_number:
+        return jsonify({"success": False, "error": "Account number is required"}), 400
+
+    account = Wallet.find_by_account_number(account_number)
+
+    wallets_collection.update_one(
+        {"_id": account["_id"]},
+        {"$set": {"total_earned": total_earned, "current_points": current_points, "link_spendless": link_spendless}}
+    )
+
+    if account:
+        return jsonify({
+            "success": True,
+            "message": "SpendLess account connected successfully",
+            "accountNumber": account_number,
+            "totalEarned": total_earned,
+            "currentPoints": current_points,
+            "linkSpendLess": link_spendless
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "error": "Account not found"
+        }), 404
+   
+@accounts_bp.route("/get-detail/spendless", methods=["GET"])
+@cross_origin(
+    origins=["http://localhost:3000", "https://depayment.vercel.app"],
+    supports_credentials=True
+)
+def details_spendless():
+    token = request.headers.get("Authorization").split(" ")[1]
+    decoded = decode_token(token)
+
+    account_number = decoded["account_number"]
+
+    if not account_number:
+        return jsonify({"success": False, "error": "Account number is required"}), 400
+
+    account = Wallet.find_by_account_number(account_number)
+
+    if account:
+        return jsonify({
+            "success": True,
+            "message": "SpendLess account connected successfully",
+            "accountNumber": account_number,
+            "totalEarned": account["total_earned"],
+            "currentPoints": account["current_points"],
+            "linkSpendLess": account["link_spendless"]
+        }), 200
+    else:
+        return jsonify({
+            "success": False,
+            "error": "Account not found"
         }), 404
