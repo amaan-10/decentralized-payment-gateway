621c5bd864ffb6bba7cddf3fdf82585ca7e8c630
Amaan <amaanshaikh.gg@gmail.com>
2025-06-24 00:19:57 +0530

    update

--- client/src/lib/url.ts
+++ client/src/lib/url.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:5000";
- export const BASE_URL = process.env.NEXT_PUBLIC_DEPAY_API_URL
+export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = process.env.NEXT_PUBLIC_DEPAY_API_URL
